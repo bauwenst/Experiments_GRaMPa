8f23adec9c6a79588f91c0d300bdeaf08da304b8
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2024-09-10 17:18:41 +0200

    Added PyProject file.

---
+++ tst/experiments/corpus_stats.py
@@ -0,0 +1,45 @@
+from tst.preamble import *
+
+import datasets
+
+from tktkt.util.iterables import take
+from tktkt.builders.english import Builder_English_BPE_native
+from fiject import StreamingMultiHistogram, BinSpec, CacheMode
+
+from tqdm.auto import tqdm
+
+from wiat.eda.sentences import tokenCounts
+
+
+def test_c4():
+    """
+    Generate a histogram of the amount of BPE tokens to expect per example in C4.
+    """
+
+    # Load data
+    N = 2_000_000
+    DATASET_NAME = ("allenai/c4", "en")
+
+    dataset = datasets.load_dataset(*DATASET_NAME, streaming=True)
+    corpus = tqdm(take(N, (example["text"] for example in dataset["train"])), total=N)
+
+    # Load tokeniser
+    tk = Builder_English_BPE_native().buildTokeniser()
+    tk_name = tk.getName()
+
+    # Graphing
+    h = StreamingMultiHistogram(f"tokencounts_{'/'.join(DATASET_NAME).replace('/', '-')}-{N}_{tk_name}",
+                                BinSpec.halfopen(minimum=0, width=50), caching=CacheMode.IF_MISSING)
+    if h.needs_computation:
+        for count in tokenCounts(tk, corpus):
+            h.add(tk_name, count)
+
+    h.commit(StreamingMultiHistogram.ArgsGlobal(
+        x_tickspacing=10000,
+        x_label="Tokens per example",
+        y_label="Examples"
+    ))
+
+
+if __name__ == "__main__":
+    test_c4()
