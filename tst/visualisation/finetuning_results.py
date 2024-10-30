ce59ff50ec0efae338e63695937a7bd842fa738e
Thomas Bauwens <thomas.bauwens@kuleuven.be>
2024-10-30 15:13:22 +0100

    Table formatter now gives control over the levels of the row hierarchy.

--- tst/visualisation/finetuning_results.py
+++ tst/visualisation/finetuning_results.py
@@ -1,14 +1,25 @@
 from tst.preamble import *
 
-from typing import Tuple, Optional
+from enum import Enum
+from typing import Tuple, Union, TypeVar, Dict, Set
+
+Number = Union[int,float]
+T = TypeVar("T")
+SortingKey = Tuple[Number,Number,Number,Number]
+TableKey = Tuple[str,str,str,str]
+Results = Dict[str, Dict[TableKey, Dict[str, float]]]
 
 from pathlib import Path
 import os
 import json
+from csv import DictReader
+import re
 
 from fiject import Table, ColumnStyle, CacheMode
 
-
+###
+# Input strings: these are used to parse the data.
+###
 GRAMPA = "GRaMPa"
 BPE = "BPE"
 ULM = "ULM"
@@ -28,24 +39,40 @@ POS  = "pos"
 NER  = "ner"
 DP   = "DP"
 
-TOKENLEVEL_NAME = "Token-level"
-SEQUENCELEVEL_NAME = "Sequence-level"
+# Regexes
+find_grampa_temperature = re.compile("t=(.+?),")
+find_grampa_length      = re.compile("l=(.+?)\)")
+find_task               = re.compile("_(.+?)_\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d")
 
-TOKENLEVEL    = [POS, NER, DP]
-SEQUENCELEVEL = [SST2, QQP, MRPC, RTE, WNLI, QNLI, MNLI, COLA, STSB]
-
+###
+# Output strings: these are used to define the strings and orders in the table.
+###
+# Ordering within one row field
 TOKENISERS = [BPEdropout, ULM, GRAMPA]
 VOCABS     = [BPE, ULM]
 
+# Ordering between the row fields
+class KeyEntry(Enum):
+    VOCAB      = 1
+    TOKENISER  = 2
+    CONSTRAINT = 3
+    SKEW       = 4
 
-def formatTaskName(task: str) -> str:
-    if task == SST2:
-        return "SST-2"
-    elif task == STSB:
-        return "STS-B"
-    else:
-        return task.upper()
+# KEY_ORDER = [KeyEntry.TOKENISER, KeyEntry.CONSTRAINT, KeyEntry.SKEW, KeyEntry.VOCAB]
+KEY_ORDER = [KeyEntry.VOCAB, KeyEntry.TOKENISER, KeyEntry.CONSTRAINT, KeyEntry.SKEW]
 
+# Column ordering
+TOKENLEVEL_NAME = "Token-level"
+SEQUENCELEVEL_NAME = "Sequence-level"
+
+FAMILY_ORDER        = [TOKENLEVEL_NAME, SEQUENCELEVEL_NAME]
+TOKENLEVEL_ORDER    = [POS, NER, DP]
+SEQUENCELEVEL_ORDER = [SST2, QQP, MRPC, RTE, WNLI, QNLI, MNLI, COLA, STSB]
+
+FAMILY_TO_TASKS = {
+    TOKENLEVEL_NAME: TOKENLEVEL_ORDER,
+    SEQUENCELEVEL_NAME: SEQUENCELEVEL_ORDER
+}
 
 TASKS_TO_METRICS_TO_RESULTS_TO_FORMATTED = {
     COLA: {"matthews_correlation": {"matthews_correlation": r"$\varphi$"}},
@@ -63,32 +90,103 @@ TASKS_TO_METRICS_TO_RESULTS_TO_FORMATTED = {
 }
 
 
-def rowSortKey(row: Tuple[str,str,str,str]) -> Tuple[int,int,float,int]:
-    name, size, skew, vocab = row
+########################################################################################################################
 
-    size = size.replace("$", "")
-    if "=" in size:
-        size = size[size.find("=")+1:]
-    size = int(size) if size else 1
+
+def orderKeyFields(tokeniser: T, constraint: T, skew: T, vocab: T) -> Tuple[T, T, T, T]:
+    """
+    Reorders the 4 key fields given by name into the order we want them in the table.
+    """
+    ordered_key = [None, None, None, None]
+    ordered_key[KEY_ORDER.index(KeyEntry.TOKENISER)]  = tokeniser
+    ordered_key[KEY_ORDER.index(KeyEntry.CONSTRAINT)] = constraint
+    ordered_key[KEY_ORDER.index(KeyEntry.SKEW)]       = skew
+    ordered_key[KEY_ORDER.index(KeyEntry.VOCAB)]      = vocab
+    return tuple(ordered_key)
+
+
+def unorderKeyFields(ordered_key: Tuple[T, T, T, T]) -> Tuple[T, T, T, T]:
+    """
+    Takes the 4 key fields as ordered in the table, and orders them in the order of the arguments to orderKeyFields().
+    """
+    return (
+        ordered_key[KEY_ORDER.index(KeyEntry.TOKENISER)],
+        ordered_key[KEY_ORDER.index(KeyEntry.CONSTRAINT)],
+        ordered_key[KEY_ORDER.index(KeyEntry.SKEW)],
+        ordered_key[KEY_ORDER.index(KeyEntry.VOCAB)]
+    )
+
+
+def stringKeyToNumberKey(row: TableKey) -> SortingKey:
+    tokeniser, constraint, skew, vocab = unorderKeyFields(row)
+
+    constraint = constraint.replace("$", "")
+    if "=" in constraint:
+        constraint = constraint[constraint.find("=")+1:]
+    constraint = int(constraint) if constraint else 1
 
     skew = skew.replace("$", "")
     if "=" in skew:
         skew = skew[skew.find("=")+1:]
     skew = float(skew) if skew else 1
 
-    return (TOKENISERS.index(name), size, -1/skew, VOCABS.index(vocab))
+    return orderKeyFields(tokeniser=TOKENISERS.index(tokeniser), constraint=constraint, skew=-1/skew, vocab=VOCABS.index(vocab))
 
 
-from csv import DictReader
-import re
+def formatTaskName(task: str) -> str:
+    if task == SST2:
+        return "SST-2"
+    elif task == STSB:
+        return "STS-B"
+    else:
+        return task.upper()
 
-find_grampa_temperature = re.compile("t=(.+?),")
-find_grampa_length      = re.compile("l=(.+?)\)")
-find_task               = re.compile("_(.+?)_\d\d\d\d-\d\d-\d\d_\d\d-\d\d-\d\d")
+
+def tableFromTasksToModelsToResults(tasks_to_models_to_results: Results):
+    """
+    Format-independent conversion to a Fiject table.
+    """
+    # Define structure
+    table = Table("finetuning", caching=CacheMode.NONE, overwriting=True)
+
+    rows: Set[Tuple[str,str,str,str]] = set()
+    for models_to_results in tasks_to_models_to_results.values():
+        models = models_to_results.keys()
+        rows.update(models)
+
+    print(tasks_to_models_to_results)
+
+    # Sort the keys and interpret the results into columns.
+    for row in sorted(rows, key=stringKeyToNumberKey):
+        for family_name in FAMILY_ORDER:
+            for task_name in FAMILY_TO_TASKS[family_name]:
+                if task_name not in tasks_to_models_to_results:
+                    continue
+                if row not in tasks_to_models_to_results[task_name]:
+                    continue
+
+                task_results_for_this = tasks_to_models_to_results[task_name][row]
+                for metric_name, result_names in TASKS_TO_METRICS_TO_RESULTS_TO_FORMATTED[task_name].items():
+                    for result_name, formatted in result_names.items():
+                        col = [family_name, formatTaskName(task_name), formatted]
+                        metric_with_result_key = f"test_{metric_name}_{result_name}"
+                        if metric_with_result_key in task_results_for_this:
+                            table.set(task_results_for_this[metric_with_result_key], list(row), col)
+
+    table.commit(
+        borders_between_columns_of_level=[0],
+        borders_between_rows_of_level=[0,1,2],
+        default_column_style=ColumnStyle(
+            do_bold_maximum=True,
+            cell_prefix=r"\tgrad[0.0][0.5][1.0]{", cell_suffix="}",
+            cell_default_if_empty=r"\cellcolor{black!10}")
+    )
 
 
-
-def resultsCsvToTable(csv_path: Path):
+def parseWandbCsv(csv_path: Path) -> Results:
+    """
+    Turn Weights&Biases CSV export into standard results format.
+    """
     # Gather data
     task_to_rows_to_results = dict()
     with open(csv_path, "r", encoding="utf-8") as handle:
@@ -106,19 +204,19 @@ def resultsCsvToTable(csv_path: Path):
                 ###
 
                 if BPE + "+" in name:
-                    key = (GRAMPA, r"$\ell_\text{min}=" + f"{l}$", fr"$\tau={t}$", BPE)
+                    key = orderKeyFields(tokeniser=GRAMPA, constraint=r"$\ell_\text{min}=" + f"{l}$", skew=fr"$\tau={t}$", vocab=BPE)
                 elif ULM + "+" in name:
-                    key = (GRAMPA, r"$\ell_\text{min}=" + f"{l}$", fr"$\tau={t}$", ULM)
+                    key = orderKeyFields(tokeniser=GRAMPA, constraint=r"$\ell_\text{min}=" + f"{l}$", skew=fr"$\tau={t}$", vocab=ULM)
                 else:
                     print("Unparsable name:", name)
                     continue
             elif BPE in name:
                 p = 0.1  # TODO: I should probably add the value of p to the pretraining shorthand, come to think of it.
-                key = (BPEdropout, "", fr"$p={p}$", BPE)
+                key = orderKeyFields(tokeniser=BPEdropout, constraint="", skew=fr"$p={p}$", vocab=BPE)
             elif ULM in name:
                 k = 64
                 a = 0.15
-                key = (ULM, fr"$k={k}$", fr"$\alpha={a}$", ULM)
+                key = orderKeyFields(tokeniser=ULM, constraint=fr"$k={k}$", skew=fr"$\alpha={a}$", vocab=ULM)
             else:
                 print("Unparsable name:", name)
                 continue
@@ -150,10 +248,13 @@ def resultsCsvToTable(csv_path: Path):
 
             task_to_rows_to_results[task_name][key] = task_results
 
-    tableFromTasksToModelsToResults(task_to_rows_to_results)
+    return task_to_rows_to_results
 
 
-def resultsFolderToTable(folder: Path):
+def parseLamotoFolder(folder: Path) -> Results:
+    """
+    Converts LaMoTO's `evaluations` folder into a results table.
+    """
     # Gather data
     rows = set()
     task_to_rows_to_results = dict()
@@ -206,48 +307,18 @@ def resultsFolderToTable(folder: Path):
             task_to_rows_to_results[task_name] = dict()
         task_to_rows_to_results[task_name][key] = task_results
 
-    tableFromTasksToModelsToResults(task_to_rows_to_results)
+    return task_to_rows_to_results
 
 
-def tableFromTasksToModelsToResults(tasks_to_models_to_results: dict):
-    # Define structure
-    table = Table("finetuning", caching=CacheMode.NONE, overwriting=True)
+def wandbToTable(csv_path: Path):
+    tableFromTasksToModelsToResults(parseWandbCsv(csv_path))
 
-    rows = set()
-    for models_to_results in tasks_to_models_to_results.values():
-        models = models_to_results.keys()
-        rows.update(models)
 
-    print(tasks_to_models_to_results)
-
-    # Sort the keys and interpret the results into columns.
-    for row in sorted(rows, key=rowSortKey):
-        for family_name, tasks in zip([TOKENLEVEL_NAME, SEQUENCELEVEL_NAME], [TOKENLEVEL, SEQUENCELEVEL]):
-            for task_name in tasks:
-                if task_name not in tasks_to_models_to_results:
-                    continue
-                if row not in tasks_to_models_to_results[task_name]:
-                    continue
-
-                task_results_for_this = tasks_to_models_to_results[task_name][row]
-                for metric_name, result_names in TASKS_TO_METRICS_TO_RESULTS_TO_FORMATTED[task_name].items():
-                    for result_name, formatted in result_names.items():
-                        col = [family_name, formatTaskName(task_name), formatted]
-                        metric_with_result_key = f"test_{metric_name}_{result_name}"
-                        if metric_with_result_key in task_results_for_this:
-                            table.set(task_results_for_this[metric_with_result_key], row, col)
-
-    table.commit(
-        borders_between_columns_of_level=[0],
-        borders_between_rows_of_level=[0,1,2],
-        default_column_style=ColumnStyle(
-            do_bold_maximum=True,
-            cell_prefix=r"\tgrad[0.0][0.5][1.0]{", cell_suffix="}",
-            cell_default_if_empty=r"\cellcolor{black!10}")
-    )
+def lamotoToTable(folder: Path):
+    tableFromTasksToModelsToResults(parseLamotoFolder(folder))
 
 
 if __name__ == "__main__":
     # EVALUATIONS = LamotoPaths.pathToEvaluations()
-    # resultsFolderToTable(EVALUATIONS)
-    resultsCsvToTable(PATH_DATA_OUT / "wandb-1.csv")
+    # lamotoToTable(EVALUATIONS)
+    wandbToTable(PATH_DATA_OUT / "wandb-2.csv")
